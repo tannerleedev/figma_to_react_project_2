cad3e8512f5ca292763bf37b3467b0d3342df54a
Tanner Lee <tannerleedev@outlook.com>
2026-07-16 09:15:43 +0330

    style: Make container configable

--- src/components/containers/BlackContainer.tsx
+++ src/components/containers/BlackContainer.tsx
@@ -1,8 +1,11 @@
 import type { ReactNode } from "react"
 
-function BlackContainer({ children }: { children: ReactNode }) {
+function BlackContainer({ children, curvePosition = "bottom" }: { children: ReactNode, curvePosition?: string }) {
+
+    const roundedStyle = curvePosition === "top" ? "rounded-t-4xl" : "rounded-b-4xl"
+
     return (
-        <div className="bg-zinc-900 text-white rounded-b-4xl">
+        <div className={`bg-zinc-900 text-white ${roundedStyle}`}>
             {children}
         </div>
     )
